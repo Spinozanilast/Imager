cf808bb44e0fe39b82172071e7f8bba3c1da552f
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2024-04-08 10:03:30 +0300

    add image converters to type coersions

---
+++ ImageConverters/IImageConverter.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace Imager.ImageConverters;
+
+public interface IImageConverter
+{
+    BitmapSource Convert(BitmapSource source, ImageType? srcImageType);
+}
---
+++ ImageConverters/ImageConverterFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace Imager.ImageConverters;
+
+public class ImageConverterFactory
+{
+    public static IImageConverter GetConverter(ImageType imageType, ColorToHalftoneCoersionType? coersionType)
+    {
+        switch (imageType)
+        {
+            case ImageType.Halftone:
+                IImageConverter imageConverter = coersionType == ColorToHalftoneCoersionType.Rgb
+                    ? new ColorToHalftoneConverterRgb()
+                    : new ColorToHalftoneConverterHsb();
+                return imageConverter;
+            case ImageType.Binary:
+                return new HalftoneToBinaryConverter();
+            default:
+                throw new ArgumentException("Unsupported image type.");
+        }
+    }
+}
